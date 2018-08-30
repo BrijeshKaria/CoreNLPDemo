e79d4e3e5e0d95065b925571c17e83a56fe4e1c0
BrijeshKaria <brijesh_karia@yahoo.com>
2018-08-30 22:40:41 +0530

    Send output to a file SampleOutput.txt

--- src/com/company/CreateRelationData.java
+++ src/com/company/CreateRelationData.java
@@ -7,6 +7,9 @@ import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -32,6 +35,8 @@ public class CreateRelationData {
         Annotation annotation = new Annotation(fileContents);
         pipeline.annotate(annotation);
         int sentNum = 0;
+        //create StringBuffer object
+        StringBuffer sbf = new StringBuffer();
         for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
             int tokenNum = 1;
             int elementNum = 0;
@@ -50,6 +55,9 @@ public class CreateRelationData {
                     String entityText = currEntityMention.toString();
                     System.out.println(sentNum+"\t"+currEntityMentionNER+"\t"+elementNum+"\t"+"O\t"+currEntityMentionTags+"\t"+
                             currEntityMentionWords+"\t"+"O\tO\tO");
+                    sbf.append(sentNum+"\t"+currEntityMentionNER+"\t"+elementNum+"\t"+"O\t"+currEntityMentionTags+"\t"+
+                            currEntityMentionWords+"\t"+"O\tO\tO");
+                    sbf.append(System.lineSeparator());
                     // update tokenNum
                     tokenNum += (currEntityMention.get(CoreAnnotations.TokensAnnotation.class).size());
                     // update entity if there are remaining entities
@@ -66,15 +74,52 @@ public class CreateRelationData {
                 } else {
                     CoreLabel token = sentence.get(CoreAnnotations.TokensAnnotation.class).get(tokenNum-1);
                     System.out.println(sentNum+"\t"+token.ner()+"\t"+elementNum+"\tO\t"+token.tag()+"\t"+token.word()+"\t"+"O\tO\tO");
+                    sbf.append(sentNum + "\t" + token.ner() + "\t" + elementNum + "\tO\t" + token.tag() + "\t" + token.word() + "\t" + "O\tO\tO");
+                    sbf.append(System.lineSeparator());
                     tokenNum += 1;
                 }
                 elementNum += 1;
             }
             sentNum++;
             System.out.println();
-            System.out.println("O\t3\tLive_In");
+            sbf.append(System.lineSeparator());
+            System.out.println("0\t3\tLive_In");
+            sbf.append("0\t3\tLive_In");
             System.out.println();
+            sbf.append(System.lineSeparator());
+            sbf.append(System.lineSeparator());
         }
+      // Write sbf to file
+        BufferedWriter bwr = null;
+        try {
+            bwr = new BufferedWriter(new FileWriter(new File("SampleOutput.txt")));
+            //write contents of StringBuffer to a file
+            bwr.write(sbf.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }     finally {
+
+            if (bwr != null) {
+                try {
+                    //flush the stream
+                    bwr.flush();
+                    //close the stream
+                    bwr.close();
+                } catch (IOException e) {
+                   // e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+        System.out.println("Content of StringBuffer written to File.");
 
     }
 }
